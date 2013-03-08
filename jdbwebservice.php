a7476b2e7279ed4cddea2c03fd4d4a5d314a6fe8
Vijai Rajpal <vjr123@gmail.com>
2013-03-07 20:26:30 -0800

    fixed query

--- jdbwebservice.php
+++ jdbwebservice.php
@@ -1,11 +1,11 @@
 <?php
-	$_POST[];
+	
 	$db = new PDO("mysql:dbname=vrajpal_seomoz;host=localhost",
 		"vrajpal","ischoolhosting");
 	$db->query("INSERT INTO Customer
-				VALUES(cid, fname, lname, sdate, oneweek, threeweek, fourweek, pend, adate, plan, notes); ");
+				VALUES('n/a', $_POST['fname'],$_POST['lname'], sdate, oneweek, threeweek, fourweek, pend, adate, 'plan', 'notes'); ");
 				
 	$db->query("INSERT INTO Email
-				VALUES(cid, email); ");
+				VALUES($_POST['email']); ");
 ?>
 
